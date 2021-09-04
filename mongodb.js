4d71dd43c82e3dc0b1a920e79470123545c36d43
xenostar <bryan.a.leewood@gmail.com>
2021-09-04 00:47:32 -0700

    main Updating mongodb for code completion.

--- mongodb.js
+++ mongodb.js
@@ -1,19 +1,84 @@
-// CRUD create read update delete
+// CRUD create read update delete 3.1.10
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
+const id = new ObjectID();
+console.log(id.id.length);
+console.log(id.toHexString().length);
+
 MongoClient.connect(
   connectionURL,
-  { useNewUrlParser: true },
+  { useNewUrlParser: true, useUnifiedTopology: true },
   (error, client) => {
     if (error) {
-      console.log("Unable to connect to database!");
+      return console.log("Unable to connect to database!");
     }
 
-    console.log("Connected correctly.");
+    const db = client.db(databaseName);
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Vikram",
+    //     age: 26,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user.");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       user: "Jen",
+    //       age: 28,
+    //     },
+    //     {
+    //       name: "Gunther",
+    //       age: 27,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents.");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "Task description 1",
+    //       completed: false,
+    //     },
+    //     {
+    //       description: "Task description 2",
+    //       completed: true,
+    //     },
+    //     {
+    //       description: "Task description 3",
+    //       completed: false,
+    //     },
+    //   ],
+    //   (error, response) => {
+    //     if (error) {
+    //       return console.log("Unable to insert tasks.");
+    //     }
+
+    //     console.log(response.ops);
+    //   }
+    // );
   }
 );
