cdf061f114b1b35f188b4e9e7040d1bddc1c0e71
xenostar <bryan.a.leewood@gmail.com>
2021-09-06 12:47:24 -0700

    main Playing with async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,14 +1,29 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("61347b5090204464074281fe")
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
+// Task.findByIdAndDelete("61347b5090204464074281fe")
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+const deleteTaskAndCount = async id => {
+  await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+
+  return count;
+};
+
+deleteTaskAndCount("6134674be1653753fb231396")
+  .then(count => {
+    console.log(count);
   })
-  .then(result => {
-    console.log(result);
-  })
-  .catch(error => {
-    console.log(error);
+  .catch(e => {
+    console.log("e", e);
   });
