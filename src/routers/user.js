a1d5b0c7c7e2cb6b8cb04fedc6b979f1f1c7cf5f
xenostar <bryan.a.leewood@gmail.com>
2021-09-06 15:24:05 -0700

    main Enabling user login route.

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,6 +13,15 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
@@ -57,7 +66,6 @@ router.patch("/users/:id", async (req, res) => {
 
     res.send(user);
   } catch (error) {
-    console.log(error);
     res.status(400).send(error);
   }
 });
