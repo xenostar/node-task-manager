8483d5c893dfd5ec8de9d2ef044fe834de4d0ebf
xenostar <bryan.a.leewood@gmail.com>
2021-09-06 20:03:52 -0700

    main Adding auth to task routes.

--- src/routers/task.js
+++ src/routers/task.js
@@ -40,7 +40,7 @@ router.get("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["description", "completed"];
   const isValidOperation = updates.every(update => allowedUpdates.includes(update));
@@ -50,7 +50,7 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
 
     if (!task) {
       return res.status(404).send();
@@ -65,9 +65,9 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id", async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
 
     if (!task) {
       return res.status(404).send();
