69d42c0e5d5f28db8083ee675ff75e302f842380
xenostar <bryan.a.leewood@gmail.com>
2021-09-06 22:51:06 -0700

    main Removing dead code.

--- src/index.js
+++ src/index.js
@@ -6,37 +6,10 @@ const userRouter = require("./routers/user");
 const app = express();
 const port = process.env.PORT || 9000;
 
-const multer = require("multer");
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error("Please upload a Word Document."));
-    }
-
-    cb(undefined, true);
-  },
-});
-
-app.post(
-  "/upload",
-  upload.single("upload"),
-  (req, res) => {
-    res.send();
-  },
-  (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
-  }
-);
-
 app.use(express.json());
 app.use(taskRouter);
 app.use(userRouter);
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}.`);
-  console.log(`Launch the app at: http://localhost:${port}`);
+  console.log(`Server is up on port ${port}. Launch the app at: http://localhost:${port}`);
 });
