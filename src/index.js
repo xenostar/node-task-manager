53fb30264691b55ca98523bbc3d7cf306379b481
xenostar <bryan.a.leewood@gmail.com>
2021-09-06 22:50:00 -0700

    main Adding image uploading abilities.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,32 @@ const userRouter = require("./routers/user");
 const app = express();
 const port = process.env.PORT || 9000;
 
+const multer = require("multer");
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please upload a Word Document."));
+    }
+
+    cb(undefined, true);
+  },
+});
+
+app.post(
+  "/upload",
+  upload.single("upload"),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
 app.use(express.json());
 app.use(taskRouter);
 app.use(userRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -83,11 +83,53 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: "avatars",
+  // dest: "avatars",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload an image."));
+    }
+
+    cb(undefined, true);
+  },
 });
 
-router.post("/users/me/avatar", upload.single("avatar"), async (req, res) => {
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+
   res.send();
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.setHeader("Content-Type", "image/jpg");
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(404).send();
+  }
 });
 
 module.exports = router;
