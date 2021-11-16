fe35259a291169db4964f787be4a72340a4ac08b
xenostar <bryan.a.leewood@gmail.com>
2021-11-15 19:12:04 -0800

    main Adding README and tests.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node Practice: Task Manager API
+
+Demo: https://xenostar-task-manager.herokuapp.com/
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,23 +1,23 @@
-const { add, calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require("../src/math");
+const { add, calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math');
 
-test("Should calculate total with tip", () => {
-  const total = calculateTip(10, 0.3);
-  expect(total).toBe(13);
+test('Should calculate total with tip', () => {
+	const total = calculateTip(10, 0.3);
+	expect(total).toBe(13);
 });
 
-test("Should calculate total with default tip", () => {
-  const total = calculateTip(10);
-  expect(total).toBe(12.5);
+test('Should calculate total with default tip', () => {
+	const total = calculateTip(10);
+	expect(total).toBe(12.5);
 });
 
-test("Should convert 32 F to 0 C", () => {
-  const total = fahrenheitToCelsius(32);
-  expect(total).toBe(0);
+test('Should convert 32 F to 0 C', () => {
+	const total = fahrenheitToCelsius(32);
+	expect(total).toBe(0);
 });
 
-test("Should convert 0 C to 32 F", () => {
-  const total = celsiusToFahrenheit(0);
-  expect(total).toBe(32);
+test('Should convert 0 C to 32 F', () => {
+	const total = celsiusToFahrenheit(0);
+	expect(total).toBe(32);
 });
 
 // test("Async test demo", done => {
@@ -27,14 +27,14 @@ test("Should convert 0 C to 32 F", () => {
 //   }, 2000);
 // });
 
-test("Should add two numbers", done => {
-  add(2, 3).then(sum => {
-    expect(sum).toBe(5);
-    done();
-  });
+test('Should add two numbers', done => {
+	add(2, 3).then(sum => {
+		expect(sum).toBe(5);
+		done();
+	});
 });
 
-test("Should add two numbers async/await", async () => {
-  const sum = await add(10, 22);
-  expect(sum).toBe(32);
+test('Should add two numbers async/await', async () => {
+	const sum = await add(10, 22);
+	expect(sum).toBe(32);
 });
